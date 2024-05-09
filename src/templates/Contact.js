1963ac7b93464f79f299dfdea99d9491cb01abea
KatherineDG <katherinedomeqg@gmail.com>
2024-05-09 20:15:35 -0300

    decoracion form contacto

--- src/templates/Contact.js
+++ src/templates/Contact.js
@@ -15,16 +15,24 @@ function Contact() {
                     </div>
                 </div>
                 <div className="form-deco">
-                <form className="form-contact">
-                    <p className="title-contact">Contactanos</p>
-                    <label className="label-form-contact">Nombre</label>
-                    <input className="input-form-contact" type="text" placeholder=""></input>
-                    <label className="label-form-contact">Email</label>
-                    <input className="input-form-contact" type="email" placeholder=""></input>
-                    <label className="label-form-contact">Mensaje</label>
-                    <textarea className="input-form-contact" placeholder=""></textarea>
-                    <button className="btn-send-contact">Enviar</button>
-                </form>
+                    <form className="form-contact">
+                        <p className="title-contact">Contactanos</p>
+                        <label className="label-form-contact">Nombre</label>
+                        <input className="input-form-contact" type="text" placeholder=""></input>
+                        <label className="label-form-contact">Email</label>
+                        <input className="input-form-contact" type="email" placeholder=""></input>
+                        <label className="label-form-contact">Mensaje</label>
+                        <textarea className="input-form-contact" placeholder=""></textarea>
+                        <button className="btn-send-contact">Enviar</button>
+
+                        <div className="contenedor-deco-brillos-contact">
+                            <img className="deco-brillo-black-izq" src="./images/icons/icon-brillo-black.png" alt="contact"></img>
+                            <div className="deco-medio-brillos-contact"></div>
+                            <img className="deco-brillo-black-der" src="./images/icons/icon-brillo-black.png" alt="contact"></img>
+                        </div>
+                    </form>
+
+
                 </div>
                 
             </div>
